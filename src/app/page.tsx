4ffe81f2236b4f5c5bfb763918efefe2e830f9e9
feyi <pheyishayor001@gmail.com>
2024-07-18 14:33:42 +0100

    commenced work on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 import "./globals.css";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <NavBar />
+      <Hero />
       <Footer />
     </>
   );
